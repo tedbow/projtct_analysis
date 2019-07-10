8000df18f04f6c97598f79fcc9016732e8cba00a
Ryan Aslett (Mixologic) <ryan@association.drupal.org>
2019-07-09 19:37:10 -0700

    need this dir

--- install_phpstan.sh
+++ install_phpstan.sh
@@ -2,6 +2,7 @@
 
 # This script will set up each of the drupal checkout directories to be ready to execute phpstan.
 cd /var/lib/drupalci/workspace/drupal-checkouts
+mkdir -p /var/lib/drupalci/workspace/phpstan-results
 git clone -s /var/lib/drupalci/drupal-checkout drupal$1
 
 cat <<EOF > /var/lib/drupalci/workspace/drupal-checkouts/drupal$1/phpstan.neon
