588874c612dde9bf3987f5ad67f1874e723654d8
Ryan Aslett (Mixologic) <ryan@association.drupal.org>
2019-08-01 16:16:36 -0700

    may as well prep the dir first, then clone 36 times

--- install_phpstan.sh
+++ install_phpstan.sh
@@ -5,26 +5,3 @@ cd /var/lib/drupalci/workspace/drupal-checkouts
 mkdir -p /var/lib/drupalci/workspace/phpstan-results
 git clone -s /var/lib/drupalci/drupal-checkout drupal$1
 
-cat <<EOF > /var/lib/drupalci/workspace/drupal-checkouts/drupal$1/phpstan.neon
-parameters:
-  customRulesetUsed: true
-  reportUnmatchedIgnoredErrors: false
-  # Ignore phpstan-drupal extension's rules.
-  ignoreErrors:
-    - '#\Drupal calls should be avoided in classes, use dependency injection instead#'
-    - '#Plugin definitions cannot be altered.#'
-    - '#Missing cache backend declaration for performance.#'
-    - '#Plugin manager has cache backend specified but does not declare cache tags.#'
-  # Migrate test fixtures kill phpstan, too much PHP.
-  excludes_analyse:
-    - */tests/fixtures/*.php
-includes:
-  - ./vendor/mglaman/phpstan-drupal/extension.neon
-  - ./vendor/phpstan/phpstan-deprecation-rules/rules.neon
-EOF
-cd /var/lib/drupalci/workspace/drupal-checkouts/drupal$1
-composer require mglaman/phpstan-drupal phpstan/phpstan-deprecation-rules --dev
-composer config repositories.patch vcs https://github.com/greg-1-anderson/drupal-finder
-composer require "webflo/drupal-finder:dev-find-drupal-drupal-root as 1.1"
-composer config prefer-stable false
-git add .;git commit -q -m "adds phpstan"
