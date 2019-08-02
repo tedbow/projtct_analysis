d1fade7689b59c13c957421e6c5b90a5a1be5951
Ryan Aslett (Mixologic) <ryan@association.drupal.org>
2019-08-01 17:03:01 -0700

    lets not hit github's api so damn much

--- d9readiness.sh
+++ d9readiness.sh
@@ -37,6 +37,7 @@ EOF
 composer require mglaman/phpstan-drupal phpstan/phpstan-deprecation-rules --dev
 composer config repositories.patch vcs https://github.com/greg-1-anderson/drupal-finder
 composer require "webflo/drupal-finder:dev-find-drupal-drupal-root as 1.1"
+composer config --unset repositories.patch
 git add .;git commit -q -m "adds phpstan"
 
 #Setup the drupal dirs
