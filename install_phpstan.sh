0f67b5389eb3472a33d02794f08b4fa27e5a98ed
Ryan Aslett (Mixologic) <ryan@association.drupal.org>
2019-09-18 23:25:03 -0700

    adds a consistent composer cache dir

--- install_phpstan.sh
+++ install_phpstan.sh
@@ -5,4 +5,4 @@ cd /var/lib/drupalci/workspace/drupal-checkouts
 mkdir -p /var/lib/drupalci/workspace/phpstan-results
 git clone -s /var/lib/drupalci/drupal-checkout drupal$1
 cd /var/lib/drupalci/workspace/drupal-checkouts/drupal$1
-composer config prefer-stable false
+COMPOSER_CACHE_DIR=/tmp/cache$1 composer config prefer-stable false
