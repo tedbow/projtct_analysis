11e2e9559873926f66bac801394f92796402801c
Ryan Aslett (Mixologic) <ryan@association.drupal.org>
2019-12-06 11:10:52 -0800

    Changes the query to be sorted by d8 project usage, excludes drupal core, and only counts modules and themes (excludes distributions)

--- phpstan_send.sh
+++ phpstan_send.sh
@@ -7,21 +7,24 @@ echo "SELECT fdfpmn.field_project_machine_name_value,
        REGEXP_REPLACE(SUBSTRING_INDEX(vl.name,'8.x-',-1), '.x','.x-dev') as \`Composer\`,
        n.type,
        fdfnmvi.field_next_major_version_info_value,
-       puwr.count,
+       puwp.count,
        vrl.release_nid
 FROM project_release_supported_versions prsv
     LEFT JOIN field_data_field_project_machine_name fdfpmn ON fdfpmn.entity_id = prsv.nid
     LEFT JOIN field_data_field_next_major_version_info fdfnmvi ON fdfnmvi.entity_id = prsv.nid
     LEFT JOIN versioncontrol_release_labels vrl ON vrl.release_nid = prsv.latest_release
     LEFT JOIN node n on n.nid = prsv.nid
-    LEFT JOIN project_usage_week_release puwr ON puwr.nid = prsv.latest_release AND puwr.timestamp = (SELECT max(timestamp) FROM project_usage_week_release)
-    LEFT JOIN project_composer_namespace_map pcnm ON pcnm.project_nid = prsv.nid AND pcnm.component_name = fdfpmn.field_project_machine_name_value
+    LEFT JOIN project_usage_week_project puwp ON puwp.nid = prsv.nid AND puwp.timestamp = (SELECT max(timestamp) FROM project_usage_week_project) and puwp.tid = 7234
+    LEFT JOIN project_composer_namespace_map pcnm ON pcnm.project_nid = fdfpmn.entity_id AND pcnm.component_name = fdfpmn.field_project_machine_name_value
     LEFT JOIN versioncontrol_labels vl ON vl.label_id = vrl.label_id
     LEFT JOIN field_data_taxonomy_vocabulary_44 fdtv44 on prsv.nid = fdtv44.entity_id
 WHERE prsv.tid = 7234
   AND pcnm.api_tid = 7234
   AND prsv.supported = 1
-  AND fdtv44.taxonomy_vocabulary_44_tid != 13032" | drush -r /var/www/drupal.org/htdocs sql-cli --extra='--skip-column-names' | sort > /tmp/projects.tsv
+  AND fdtv44.taxonomy_vocabulary_44_tid != 13032
+  AND prsv.nid != 3060
+  AND n.type IN ('project_module', 'project_theme')
+ORDER BY puwp.count DESC" | drush -r /var/www/drupal.org/htdocs sql-cli --extra='--skip-column-names' | sort > /tmp/projects.tsv
 #head /tmp/testfile.tsv > /tmp/projects.tsv
 
 curl https://dispatcher.drupalci.org/job/phpstan/build -F file0=@/tmp/projects.tsv -F json='{"parameter": [{"name":"projects.tsv", "file":"file0"}]}' -F token=${TOKEN}
