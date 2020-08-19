6a7a300b0d6ea288820f4eed16fe91faab2806c6
Ryan Aslett (Mixologic) <ryan@association.drupal.org>
2020-08-19 09:47:01 -0700

    Quiet down the checkout noise

--- install_phpstan.sh
+++ install_phpstan.sh
@@ -3,6 +3,6 @@
 # This script will set up each of the drupal checkout directories to be ready to execute phpstan.
 cd /var/lib/drupalci/workspace/drupal-checkouts
 mkdir -p /var/lib/drupalci/workspace/phpstan-results
-git clone -s /var/lib/drupalci/drupal-checkout drupal$1
+git clone -q -s /var/lib/drupalci/drupal-checkout drupal$1
 cd /var/lib/drupalci/workspace/drupal-checkouts/drupal$1
 COMPOSER_CACHE_DIR=/tmp/cache$1 composer config prefer-stable false
