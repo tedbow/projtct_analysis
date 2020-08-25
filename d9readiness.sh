b754bd427e4a2b653fe16eae9a0dc737ee570e64
Ryan Aslett (Mixologic) <ryan@association.drupal.org>
2020-08-25 11:03:46 -0700

    Issue #3167265: moves most of the setup to the docker container

--- d9readiness.sh
+++ d9readiness.sh
@@ -3,74 +3,11 @@ set -eux
 
 export PHPSTAN_RESULT_DIR="/var/lib/drupalci/workspace/phpstan-results"
 
-#This file is intended to be executed on the testbots.
-sudo composer selfupdate
-
-# Upgrade to php7.2.
-# This must happen after updating composer or composer must be removed and reinstalled.
-#sudo /var/lib/drupalci/workspace/infrastructure/stats/project_analysis/upgrade_php.sh
+#This file is intended to be executed on the testbot docker container.
 
 rm -rf /var/lib/drupalci/workspace/phpstan-results || true
 
 PROC_COUNT=`grep processor /proc/cpuinfo |wc -l`
-sudo dpkg -i /var/lib/drupalci/workspace/infrastructure/stats/project_analysis/parallel_20190622_all.deb
-
-#Ensure we've got the latest drupal.
-cd /var/lib/drupalci/drupal-checkout
-git config --global user.email "git@drupal.org"
-git config --global user.name "Drupalci Testbot"
-git fetch
-git checkout 8.9.x
-git pull
-rm -rf vendor
-
-cat <<EOF > phpstan.neon
-parameters:
-  drupal:
-    drupal_root: %currentWorkingDirectory%
-  customRulesetUsed: true
-  reportUnmatchedIgnoredErrors: false
-  # Ignore phpstan-drupal extension's rules.
-  ignoreErrors:
-    - '#\Drupal calls should be avoided in classes, use dependency injection instead#'
-    - '#Plugin definitions cannot be altered.#'
-    - '#Missing cache backend declaration for performance.#'
-    - '#Plugin manager has cache backend specified but does not declare cache tags.#'
-  # Migrate test fixtures kill phpstan, too much PHP.
-  excludes_analyse:
-    - */tests/fixtures/*.php
-includes:
-  - ./vendor/mglaman/phpstan-drupal/extension.neon
-  - ./vendor/phpstan/phpstan-deprecation-rules/rules.neon
-EOF
-composer require palantirnet/drupal-rector:0.5.6 --dev
-composer -q require drupal/upgrade_status:2.8
-
-
-# Use the local package for updating info.yml files. This repo can not be symlink because of autoloaders.
-composer config repositories.local '{"type": "path", "url": "/var/lib/drupalci/workspace/infrastructure/stats/project_analysis/project_analysis_utils", "options": { "symlink": false}}'
-composer -q require drupalorg_infrastructure/project_analysis_utils --no-progress
-composer -q require drush/drush:9.7.2 --no-progress
-
-#composer config repositories.patch vcs https://github.com/greg-1-anderson/drupal-finder
-#composer require "webflo/drupal-finder:dev-find-drupal-drupal-root as 1.1"
-#composer config --unset repositories.patch
-find vendor -name .git -exec rm -rf {} \; || true
-cp /var/lib/drupalci/workspace/infrastructure/stats/project_analysis/rector.yml rector.yml
-cp /var/lib/drupalci/workspace/infrastructure/stats/project_analysis/rector-no-tests.yml rector-no-tests.yml
-sudo chmod 777 sites/default
-./vendor/bin/drush si --db-url=sqlite://sites/default/files/.ht.sqlite -y
-./vendor/bin/drush en upgrade_status -y
-git add sites/default/files/.ht.sqlite
-git add .;git commit -q -m "adds phpstan and drupal-rector and sqlite"
-
-# Save the composer.lock file so that the versions of drupal-rector and upgrade_status are available in the results.
-mkdir /var/lib/drupalci/workspace/phpstan-results
-cp /var/lib/drupalci/drupal-checkout/composer.lock /var/lib/drupalci/workspace/phpstan-results/drupal-composer.lock.json
-
-#Setup the drupal dirs
-rm -rf /var/lib/drupalci/workspace/drupal-checkouts
-mkdir -p /var/lib/drupalci/workspace/drupal-checkouts
 parallel /var/lib/drupalci/workspace/infrastructure/stats/project_analysis/install_phpstan.sh {} ::: $(seq -s' ' 1 ${PROC_COUNT})
 # Run the analyzers.
 # 1/2/3/4/8 correspond to the columns in the project listing file which should take the
--- install_phpstan.sh
+++ install_phpstan.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
 # This script will set up each of the drupal checkout directories to be ready to execute phpstan.
-cd /var/lib/drupalci/workspace/drupal-checkouts
-mkdir -p /var/lib/drupalci/workspace/phpstan-results
-git clone -q -s /var/lib/drupalci/drupal-checkout drupal$1
-cd /var/lib/drupalci/workspace/drupal-checkouts/drupal$1
-COMPOSER_CACHE_DIR=/tmp/cache$1 composer config prefer-stable false
+git clone --quiet --shared /var/lib/drupalci/drupal-checkout /var/lib/drupalci/workspace/drupal-checkouts/drupal$1
+COMPOSER_CACHE_DIR=/tmp/cache$1 composer --working-dir /var/lib/drupalci/workspace/drupal-checkouts/drupal$1 config prefer-stable false
