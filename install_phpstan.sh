11e73b340dd19b6709452933e9aa7c4c5a8b21bf
Ryan Aslett (Mixologic) <ryan@association.drupal.org>
2020-08-25 14:37:05 -0700

    working dir might need an =

--- install_phpstan.sh
+++ install_phpstan.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+set -eux
 
 # This script will set up each of the drupal checkout directories to be ready to execute phpstan.
 git clone --quiet --shared /var/lib/drupalci/drupal-checkout /var/lib/drupalci/workspace/drupal-checkouts/drupal$1
-COMPOSER_CACHE_DIR=/tmp/cache$1 composer --working-dir /var/lib/drupalci/workspace/drupal-checkouts/drupal$1 config prefer-stable false
+COMPOSER_CACHE_DIR=/tmp/cache$1 composer --working-dir=/var/lib/drupalci/workspace/drupal-checkouts/drupal$1 config prefer-stable false
