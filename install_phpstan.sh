ce6b907cf6b5b0f74e29f0a35a9f5872b6864d60
Ryan Aslett (Mixologic) <ryan@association.drupal.org>
2020-12-02 08:52:00 -0800

    Updates the cache to be under drupal-checkouts

--- install_phpstan.sh
+++ install_phpstan.sh
@@ -3,4 +3,4 @@ set -eux
 
 # This script will set up each of the drupal checkout directories to be ready to execute phpstan.
 git clone --quiet --shared /var/lib/drupalci/drupal-checkout /var/lib/drupalci/workspace/drupal-checkouts/drupal$1
-COMPOSER_CACHE_DIR=/tmp/cache$1 composer --working-dir=/var/lib/drupalci/workspace/drupal-checkouts/drupal$1 config prefer-stable false
+COMPOSER_CACHE_DIR=/var/lib/drupalci/workspace/drupal-checkouts/cache$1 composer --working-dir=/var/lib/drupalci/workspace/drupal-checkouts/drupal$1 config prefer-stable false
