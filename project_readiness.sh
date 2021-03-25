b59d70bdc74012d4ad34d23a01a9bf5d8649f189
Ryan Aslett (Mixologic) <ryan@association.drupal.org>
2021-03-24 17:16:09 -0700

    updates job

--- project_readiness.sh
+++ project_readiness.sh
@@ -2,11 +2,11 @@
 set -eux
 
 export PHPSTAN_RESULT_DIR="/var/lib/drupalci/workspace/phpstan-results"
-
+$DRUPAL_BRANCH = $1
 #This file is intended to be executed on the testbot docker container.
 
 PROC_COUNT=`grep processor /proc/cpuinfo |wc -l`
-parallel /var/lib/drupalci/workspace/infrastructure/stats/project_analysis/install_phpstan.sh {} ::: $(seq -s' ' 1 ${PROC_COUNT})
+parallel /var/lib/drupalci/workspace/infrastructure/stats/project_analysis/install_phpstan.sh {} ${DRUPAL_BRANCH} ::: $(seq -s' ' 1 ${PROC_COUNT})
 # Run the analyzers.
 # 1/2/3/4/8 correspond to the columns in the project listing file which should take the
 # following form:
