2629d9c7a2e0ab262e338441d33a206d9a049938
bjorn <bjorn@swis.nl>
2022-06-05 21:03:17 +0200

    Add simple way to run locally using docker

---
+++ docker_run_locally.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Build the container locally, nice and fresh
+docker build docker/. -t infrastructure/project_analysis:latest
+
+# Remove existing container, usefull if this is a second run
+docker rm "project_analysis_local" -f
+
+# Start the container
+docker run -d --name project_analysis_local infrastructure/project_analysis
+
+# Copy the test set into the container
+docker cp ./project_analysis_utils/tests/project_list_files/projects_d10.tsv project_analysis_local:/var/lib/drupalci/workspace/projects.tsv
+
+# Execute the readiness scripts
+docker exec project_analysis_local /var/lib/drupalci/workspace/infrastructure/stats/project_analysis/project_readiness.sh
+
+# Copy the results to your local machine
+docker cp project_analysis_local:/var/lib/drupalci/workspace/phpstan-results .
+
+# Want to explore this container? Run the following:
+# docker exec -it project_analysis_local /bin/bash
--- project_analysis_utils/tests/project_list_files/README.md
+++ project_analysis_utils/tests/project_list_files/README.md
@@ -1,13 +1,14 @@
 These projects.*.tsv files are not associated with a test class but can be used to test:
-`stats/project_analysis/d9readiness.sh`
+`stats/project_analysis/project_readiness.sh`
 
 These files may stop to work as test as the projects include make commits fixing problems.environment_indicator
 
 Rename a file `projects.tsv` move it to:
 `/var/lib/drupalci/workspace/projects.tsv`
 
-Then run `stats/project_analysis/d9readiness.sh`
+Then run `stats/project_analysis/project_readiness.sh`
 
 File list:
 * `projects_machine_name.tsv`: Used to test where the project name the module machine name are not the same or the main info.yml file is not in base directory.
 * `projects.info_updates.tsv`: Used to test updates to info.yml. @see \InfoUpdater\Tests\Unit\InfoUpdaterTest::providerUpdateInfoNew for expected updates
+
