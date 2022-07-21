94d39bb38eb81ca9cdef071b816f20ef62198cfc
bjorn <bjorn@swis.nl>
2022-07-21 21:03:34 +0200

    If PROC_COUNT is set, also run parallel only for so many threads

--- project_readiness.sh
+++ project_readiness.sh
@@ -42,4 +42,4 @@ parallel /var/lib/drupalci/workspace/infrastructure/stats/project_analysis/prepa
 # Project name<tab>Composer Namespace<tab>Branch<tab>Project type<tab>Drupal 10 readiness text<tab>project usage count<tab>release node id<tab>concat core_version_requirement
 # blazy	blazy	1.x-dev	project_module	NULL	55069	2663392	blazy_ui:subcomponent:"",blazy:primary:"^8 || ^9"
 echo "Starting analysis with  ${PROC_COUNT} threads"
-time parallel --colsep '\t' --timeout 900 /var/lib/drupalci/workspace/infrastructure/stats/project_analysis/analyzer.sh "{1}" "{2}" "{3}" "{4}" "{%}" "{8}" :::: /var/lib/drupalci/workspace/projects.tsv 2>&1 > /var/lib/drupalci/workspace/analyzer_output.log
+time parallel -j${PROC_COUNT} --colsep '\t' --timeout 900 /var/lib/drupalci/workspace/infrastructure/stats/project_analysis/analyzer.sh "{1}" "{2}" "{3}" "{4}" "{%}" "{8}" :::: /var/lib/drupalci/workspace/projects.tsv 2>&1 > /var/lib/drupalci/workspace/analyzer_output.log
