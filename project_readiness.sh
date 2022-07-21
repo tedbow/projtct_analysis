359b04c5b2100e014992c78ab396b6589ad26dba
bjorn <bjorn@swis.nl>
2022-07-21 19:55:33 +0200

    more info

--- project_readiness.sh
+++ project_readiness.sh
@@ -30,10 +30,10 @@ composer --working-dir=/var/lib/drupalci/drupal-checkout require drupalorg_infra
 git -C /var/lib/drupalci/drupal-checkout add .
 git -C /var/lib/drupalci/drupal-checkout commit -q -m "Update project analysis internal library"
 
-echo "Preparing workspace"
 # Set up as many workspaces for running phpstan as many processor cores we have.
 #PROC_COUNT=`grep processor /proc/cpuinfo |wc -l`
 PROC_COUNT=2
+echo "Preparing ${PROC_COUNT} workspaces"
 parallel /var/lib/drupalci/workspace/infrastructure/stats/project_analysis/prepare_workspace.sh {} ::: $(seq -s' ' 1 ${PROC_COUNT})
 
 # Run the analyzers in parallel in each workspace we created.
@@ -41,5 +41,5 @@ parallel /var/lib/drupalci/workspace/infrastructure/stats/project_analysis/prepa
 # following form:
 # Project name<tab>Composer Namespace<tab>Branch<tab>Project type<tab>Drupal 10 readiness text<tab>project usage count<tab>release node id<tab>concat core_version_requirement
 # blazy	blazy	1.x-dev	project_module	NULL	55069	2663392	blazy_ui:subcomponent:"",blazy:primary:"^8 || ^9"
-echo "Starting analysis"
+echo "Starting analysis with  ${PROC_COUNT} threads"
 time parallel --colsep '\t' --timeout 900 /var/lib/drupalci/workspace/infrastructure/stats/project_analysis/analyzer.sh "{1}" "{2}" "{3}" "{4}" "{%}" "{8}" :::: /var/lib/drupalci/workspace/projects.tsv 2>&1 > /var/lib/drupalci/workspace/analyzer_output.log
